c3a0d24554489481583176e31913d80d3f3aeb82
alexscub <alexscub1@gmail.com>
2019-11-22 00:16:33 +0200

    Updates

---
+++ precache-manifest.bbf20b3bbca0b11d36b5d2fa357921be.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "73aaffbf0029dd1cb2a4abf75cd5eed9",
+    "url": "/goit-react-hw-04-movies/index.html"
+  },
+  {
+    "revision": "c599d3f8e97ac08d7e80",
+    "url": "/goit-react-hw-04-movies/static/css/main.04fa54f2.chunk.css"
+  },
+  {
+    "revision": "0e1a882e87524cc196bd",
+    "url": "/goit-react-hw-04-movies/static/js/2.6d6dbdfa.chunk.js"
+  },
+  {
+    "revision": "c599d3f8e97ac08d7e80",
+    "url": "/goit-react-hw-04-movies/static/js/main.b21e9c60.chunk.js"
+  },
+  {
+    "revision": "c0fe785ba10d04f79489",
+    "url": "/goit-react-hw-04-movies/static/js/runtime-main.92c73491.js"
+  }
+]);
